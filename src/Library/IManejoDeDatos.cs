07e84f8ece7669078eeca2a7c94d178757d0ec8d
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-10-27 18:46:13 -0300

    correciones 2

--- src/Library/IManejoDeDatos.cs
+++ src/Library/IManejoDeDatos.cs
@@ -6,6 +6,7 @@ namespace Library
         public void GuardarInfo(Oferta oferta); //no esta creada la clase oferta aun
 
         public void DevolverInfo(); //no se de que manera buscar el registro si con un indice o algo relacionado a la empresa
-    }      /* //no se de que manera buscar el registro si con un indice
+    }
+}          /* //no se de que manera buscar el registro si con un indice
                  o algo relacionado a la empresa */
                  
