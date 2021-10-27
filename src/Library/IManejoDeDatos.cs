2f8539de9552f6b62ff54368070fa600a440f1ea
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-10-27 18:47:08 -0300

    agregado comentarios

--- src/Library/IManejoDeDatos.cs
+++ src/Library/IManejoDeDatos.cs
@@ -5,7 +5,7 @@ namespace Library
     {
         public void GuardarInfo(Oferta oferta); //no esta creada la clase oferta aun
 
-        public void DevolverInfo(); //no se de que manera buscar el registro si con un indice o algo relacionado a la empresa
+        public void DevolverInfo(); 
     }
 }          /* //no se de que manera buscar el registro si con un indice
                  o algo relacionado a la empresa */
