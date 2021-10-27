7f5d2e00fc30284f85fcfd1c43c01e2ff48e7590
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-10-27 14:07:04 -0300

    Interfaz IManejoDeDatos

---
+++ src/Library/IManejoDeDatos.cs
@@ -0,0 +1,14 @@
+
+namespace Library
+{
+    /// <summary>
+    /// Esta clase representa un Emprendedor
+    /// </summary>
+    public interface IManejoDeDatos
+    {
+        public void GuardarInfo(Oferta oferta); //no esta creada la clase oferta aun
+
+        public void DevolverInfo(); //no se de que manera buscar el registro si con un indice o algo relacionado a la empresa
+    }      /* //no se de que manera buscar el registro si con un indice
+                 o algo relacionado a la empresa */
+                 
