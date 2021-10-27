54086e4f74e7ce02cfa4f7a0888c95608d3c3e61
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-10-27 20:39:36 -0300

    agregado rubro

---
+++ src/Library/Rubro.cs
@@ -0,0 +1,17 @@
+namespace Library
+{
+    /// <summary>
+    /// Clase que representa los rubros
+    /// </summary>
+    public class Rubro
+    {
+        public string rubros;
+        public string clasificacion;
+
+    public Rubro(string rubro1, string clasificacion)
+    {
+        this.rubros = rubro1;
+        this.clasificacion = clasificacion;
+    }
+    }
+}
