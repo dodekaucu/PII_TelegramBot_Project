f93d5e99c576f336887c9cbd1112d8f022f92efc
unknown <filardisafa@gmail.com>
2021-11-15 19:00:59 -0300

    Agrego AdminHandlers y HistorialUsuarioHandler

---
+++ src/Library/Handlers/AdminHandlers/AddAdminHandler.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using Telegram.Bot.Types;
+using Library;
+
+namespace Ucu.Poo.TelegramBot
+{
+    public class AddAdminHandler : BaseHandler
+    {
+        private Contenedor contenedor;
+        public AddAdminHandler(BaseHandler next, Contenedor contenedor) : base(next)
+        {
+            this.Keywords = new string[] { "/AñadirAdmin" };
+
+            this.contenedor = contenedor;
+        }
+
+        protected override bool InternalHandle(IMessage message, out string response)  
+        {
+            if (this.CanHandle(message))
+            {
+                if (this.contenedor.Administradores.Contains(message.ID))
+                {
+                    string newAdmin = message.Text.Replace("/AñadirAdmin ","").Trim();
+                    this.contenedor.AddAdministrador(newAdmin); 
+                    response = "Se ha asignado como admin al usuario con ID: " + newAdmin;
+                    return true;
+                }
+                else
+                {
+                    response = "Este comando solo puede ser ejecutado por un administrador";
+                    return true;
+                }
+            }
+
+            response = string.Empty;
+            return false;
+        }
+
+        protected override bool CanHandle(IMessage message)
+        {
+            if (this.Keywords == null || this.Keywords.Length == 0)
+            {
+                throw new InvalidOperationException("No hay palabras clave que puedan ser procesadas");
+            }
+
+            return this.Keywords.Any(s => message.Text.StartsWith(s, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
